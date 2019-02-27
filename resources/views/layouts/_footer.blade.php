d8ba5ba5e5089e843075840ec4fa563d0ed1657c
k <710312372@qq.com>
2019-02-27 10:40:59 +0000

    静态文件浏览缓存问题 and 切割头部和尾部子视图2

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,14 +1,9 @@
- <footer class="footer">
-    <small class="slogon">
-      <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-      <a href="http://estgroupe.com/">
-        优帆远扬 | 创造不息，交付不止
-      </a>
-    </small>
-    <nav>
-      <ul>
-        <li><a href="/about">关于</a></li>
-      </ul>
-    </nav>
-  </footer>
-
+<footer class="footer">
+<img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+<a href="https://laravel-china.org/courses" target=_blank>
+刻意练习，每日精进
+</a>
+<div class="float-right">
+<a href="/about" >关于</a>
+</div>
+</footer>
