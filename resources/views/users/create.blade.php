f1977a0faba609bbe0904b6747462faf38effe30
k <710312372@qq.com>
2019-03-01 10:28:27 +0000

    增加 CSRF 验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
 @section('title', '注册')
 @section('content')
+
 <div class="offset-md-2 col-md-8">
 <div class="card ">
   <div class="card-header">
@@ -8,6 +9,7 @@
 </div>
 <div class="card-body">
 <form method="POST" action="{{ route('users.store') }}">
+  {{ csrf_field() }}
 <div class="form-group">
 <label for="name">名称：</label>
 <input type="text" name="name" class="form-control" value="{{ old('name') }}">
