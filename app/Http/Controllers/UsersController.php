381e94daf15d3ff2a420b04cf5a1dc639fbba390
k <710312372@qq.com>
2019-03-05 07:10:06 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,12 @@ use App\Models\User;
 class UsersController extends Controller
 {
 	//
+    public function __construct()
+    {
+        $this->middleware('auth', [
+        'except' => ['show', 'create', 'store']
+        ]);
+    }
 
 	public function create()
 	{
@@ -39,12 +45,14 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+         $this->authorize('update', $user);
          return view('users.edit', compact('user'));
     }
 
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
         'name' => 'required|max:50',
         'password' => 'nullable|confirmed|min:6'
