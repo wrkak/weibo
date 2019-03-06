0a2779e8b0fb69e8cd9db619012bb686f385ddd4
k <710312372@qq.com>
2019-03-06 09:29:26 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,5 +52,9 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 
 }
